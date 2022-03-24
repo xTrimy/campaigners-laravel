b7ce55742386bfb494271aecba6b82049436475d
xTrimy <Mohamed.ashraf881999@gmail.com>
2022-03-24 03:32:10 +0200

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,6 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
         'university_id',
         'phone',
         'faculty_id',

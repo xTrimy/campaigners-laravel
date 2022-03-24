17bfdd479a2be6cb378eab085d8c0c59c33c0c25
xTrimy <Mohamed.ashraf881999@gmail.com>
2022-03-24 03:31:32 +0200

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,8 +18,10 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-
+        $user = User::where('email',$request->email)->first();
+        dd($user??null);
         $credentials  = $request->only(['email','password']);
+        
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             return redirect()->route('leaf.home');
